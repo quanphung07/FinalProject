b924795c12808db866e9e4de5f21884eb118ca3c
phungquan07 <quanbka00@gmail.com>
2020-12-21 11:57:32 +0700

    add Cars.cs

---
+++ Models/Cars.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace test1.Models
+{
+    public partial class Cars
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Price { get; set; }
+    }
+}
